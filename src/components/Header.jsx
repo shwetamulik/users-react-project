4616f84a6028df1868f4e90b584454f80be0d7b8
shwetamulik <shwetamulik7@gmail.com>
2024-01-28 18:47:11 +0530

    removed unwanted imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import {Link} from 'react-router-dom';
 import useState from 'react';
-import Modal from './Modal';
 export default  function Header(){
     // const [isOpen, setIsOpen] = useState(false)
     const tabList = ['About', 'Contact']
